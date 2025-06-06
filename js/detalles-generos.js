3da9d9b51280215a813fc2158db0c60d518497ec
MSpeier <mspeier@udesa.com.ar>
2025-06-06 14:51:58 -0300

    detalles finales de generos

--- js/detalles-generos.js
+++ js/detalles-generos.js
@@ -1,51 +1,65 @@
-let DetallesGeneros= ''
-let Dcontainer= document.querySelector('.peliculas')
-let apiKey = "a2ebd75bfce21a517850620a286006ec";
+let DetallesGeneros = '';
+let Dcontainer = document.querySelector('.peliculas');
 let queryString = location.search;
 let datos = new URLSearchParams(queryString);
 let id = datos.get("id");
 let imagenBase = "https://image.tmdb.org/t/p/w500";
+let apiKey = "a2ebd75bfce21a517850620a286006ec";
+
 
 fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=es-ES&with_genres=${id}`)
-    .then(function(response){
-    return response.json()
-})
-    .then(function(data){
-        let peliculas= data.results
-    for(let i = 0 ; i < 5; i++){
-DetallesGeneros += `
-            <article>
-            <a href="detalles-Pelicula.html?id=${peliculas[i].id}">
-              <img src="${imagenBase}${peliculas[i].poster_path}" alt="${peliculas[i].title}">
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let peliculas = data.results;
+
+    for (let i = 0; i < 5; i++) {
+      let pelicula = peliculas[i];
+
+      if (pelicula && pelicula.title && pelicula.poster_path) {
+        DetallesGeneros += `
+          <article>
+            <a href="detalles-Pelicula.html?id=${pelicula.id}">
+              <img src="${imagenBase}${pelicula.poster_path}" alt="${pelicula.title}">
             </a>
-            <p>${peliculas[i].title} (${peliculas[i].release_date})</p>
+            <p>${pelicula.title} (${pelicula.release_date})</p>
           </article>
-`
-}
-Dcontainer.innerHTML = DetallesGeneros
-})
-.catch(function (error) {
-console.error('Error al obtener los detalles de la película:', error);
-});
+        `;
+      }
+    }
+
+    Dcontainer.innerHTML = DetallesGeneros;
+  })
+  .catch(function(error) {
+    console.log("Error al obtener películas:", error);
+  });
+
 
 fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&language=es-ES&with_genres=${id}`)
-    .then(function(response){
-    return response.json()
-})
-    .then(function(data){
-        let peliculas= data.results
-    for(let i = 0 ; i < 5; i++){
-DetallesGeneros += `
-            <article>
-            <a href="detalles-Pelicula.html?id=${peliculas[i].id}">
-              <img src="${imagenBase}${peliculas[i].poster_path}" alt="${peliculas[i].title}">
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let series = data.results;
+
+    for (let i = 0; i < 5; i++) {
+      let serie = series[i];
+
+      if (serie && serie.name && serie.poster_path) {
+        DetallesGeneros += `
+          <article>
+            <a href="detalles-Series.html?id=${serie.id}">
+              <img src="${imagenBase}${serie.poster_path}" alt="${serie.name}">
             </a>
-            <p>${peliculas[i].title} (${peliculas[i].release_date})</p>
+            <p>${serie.name} (${serie.first_air_date})</p>
           </article>
-`
-}
-Dcontainer.innerHTML = DetallesGeneros
-})
-.catch(function (error) {
-console.error('Error al obtener los detalles de la película:', error);
-});
+        `;
+      }
+    }
+
+    Dcontainer.innerHTML = DetallesGeneros;
+  })
+  .catch(function(error) {
+    console.log("Error al obtener series:", error);
+  });
