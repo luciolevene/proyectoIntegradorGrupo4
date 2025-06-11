ddca9bbd8909b134f2783a6d5b7ad84d682e2f8d
MSpeier <mspeier@udesa.com.ar>
2025-06-11 00:53:52 -0300

    ultimos cambios de resultados

--- js/resultados.js
+++ js/resultados.js
@@ -1,51 +1,65 @@
-let apiKey = "a2ebd75bfce21a517850620a286006ec";
-let imagenBase = "https://image.tmdb.org/t/p/w500";
-let urlBase = "https://api.themoviedb.org/3";
-
 let queryString = location.search;
-let datos = new URLSearchParams(queryString);
+let queryserie = new URLSearchParams(queryString);
+let buscar = queryserie.get("q");
+let seleccion = queryserie.get("tipo");
+let apiKey = "a2ebd75bfce21a517850620a286006ec";
+let info = document.querySelector(".peliculas");
+let resultsHTML = "";
+let imagenBase = "https://image.tmdb.org/t/p/w500";
 
-let tipo = datos.get("tipo"); 
-let resultado = document.querySelector(".peliculas");
+if (seleccion == "movie") {
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${buscar}`;
 
-
-if (tipo === "pelicula") {
-  fetch(`${urlBase}/movie/popular?api_key=${apiKey}&language=es`)
-    .then(function (response) {
+  fetch(url)
+    .then(function(response) {
       return response.json();
     })
-    .then(function (data) {
-      let contenido = "";
-      for (let i = 0; i < data.results.length; i++) {
-        contenido += `
-             <article>
-                <a href="detalles-Pelicula.html?id=${data.results[i].id}">
-             <img src="${imagenBase}${data.results[i].poster_path}" alt="${data.results[i].title}">
-             </a>
-                <p>${data.results[i].title} (${data.results[i].release_date})</p>
-         </article>`
-      };
-      resultado.innerHTML = contenido;
-    });
-
-
-} else if (tipo === "serie") {
-  fetch(`${urlBase}/tv/popular?api_key=${apiKey}&language=es`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      let contenido = ""; 
-      for (let i = 0; i < data.results.length; i++) {
-        contenido += `
-          <article>
-          <a href="detalles-Series.html?id=${data.results[i].id}">
-           <img src="${imagenBase}${data.results[i].poster_path}" alt="${data.results[i].name}">
-       </a>
-          <p>${data.results[i].name} (${data.results[i].first_air_date})</p>
-      </article>`;
+    .then(function(data) {
+      let movies = data.results;
+      for (let i = 0; i < movies.length; i++) {
+        resultsHTML += `
+          <a class="linkdetail" href="./detalles-Pelicula.html?id=${movies[i].id}">
+            <div class="peliculas">
+              <img class="imgpelis" src="https://image.tmdb.org/t/p/w500${movies[i].poster_path}" alt="${movies[i].title}">
+              <p class="textseccion">
+                <span class="negrita">${movies[i].original_title}</span> (${movies[i].release_date})
+              </p>
+            </div>
+          </a>
+        `;
       }
-      resultado.innerHTML = contenido;
+      info.innerHTML = resultsHTML;
+    })
+    .catch(function(error) {
+      console.error("Ocurrió un error:", error.message);
     });
-    
+
+} else if (seleccion == "tv") {
+  let url = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${buscar}`;
+
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let series = data.results;
+      for (let i = 0; i < series.length; i++) {
+        resultsHTML += `
+          <a class="linkdetail" href="./detalles-Series.html?id=${series[i].id}">
+            <div class="peliculas nuevo">
+              <img class="imgpelis" src="https://image.tmdb.org/t/p/w500${series[i].poster_path}" alt="${series[i].name}">
+              <p class="textseccion">
+                <span class="negrita">${series[i].name}</span> (${series[i].first_air_date})
+              </p>
+            </div>
+          </a>
+        `;
+      }
+      info.innerHTML = resultsHTML;
+    })
+    .catch(function(error) {
+      console.error("Ocurrió un error:", error.message);
+    });
+} else {
+  info.innerHTML = "<p>No se especificó correctamente el tipo de búsqueda.</p>";
 }
